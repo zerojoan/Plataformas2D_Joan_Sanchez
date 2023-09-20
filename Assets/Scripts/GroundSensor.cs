44f429b672b7d0112ee0159586bb25b6018f9b28
zerojoan <zerojoan117@gmail.com>
2023-09-20 11:42:17 +0200

    andar saltar bien

--- Assets/Scripts/GroundSensor.cs
+++ Assets/Scripts/GroundSensor.cs
@@ -6,21 +6,21 @@ public class GroundSensor : MonoBehaviour
 {
     public bool _isGrounded;
     
-     void OntriggerEnter2D(Collider2D other)
+     void OnTriggerEnter2D(Collider2D other)
      {
         if(other.gameObject.layer == 6)
         _isGrounded = true;
 
      }
 
-     void OntriggerExit2D(Collider2D other)
+     void OnTriggerExit2D(Collider2D other)
      {
         if(other.gameObject.layer == 6)
         _isGrounded = false;
 
      }
 
-     void OntriggerStay2D(Collider2D other)
+     void OnTriggerStay2D(Collider2D other)
      {
         if(other.gameObject.layer == 6)
         _isGrounded = true;
