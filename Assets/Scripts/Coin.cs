171029fa94c230a9c2fb6faa8ef39be5685c554e
zerojoan <zerojoan117@gmail.com>
2023-11-22 12:30:13 +0100

    monedas

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int value;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            CoinCounter.instance.IncreaseCoins(value);
+        }
+    }
+}
---
+++ Assets/Scripts/CoinCounter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinCounter : MonoBehaviour
+{
+    public static CoinCounter instance;
+
+    public TMP_Text coinText;
+    public int currentCoins = 0;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        coinText.text = "COINS: " + currentCoins.ToString();
+    }
+
+    public void IncreaseCoins(int v)
+    {
+        currentCoins += v;
+        coinText.text = "COINS: " + currentCoins.ToString();
+    }
+}
